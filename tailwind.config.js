0c565e06d65a3ba4ef4a375e1255354a19d2c049
lalit_h <lalithireofficial@gmail.com>
2024-10-06 20:03:20 +0530

    Fixes #9

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,css,js,jsx,ts,tsx}',  // Adjusted according to our folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
