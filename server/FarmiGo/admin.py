07c2fabb7ce1e7752149106db0578d6a465c5427
lalit_h <lalithireofficial@gmail.com>
2024-10-06 14:51:29 +0530

    Vegetable Created

--- server/FarmiGo/admin.py
+++ server/FarmiGo/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 
-# Register your models here.
+# Our model register here for adminsite
+from .models import Vegetable
+admin.site.register(Vegetable)
+
+#for log entry which will show us which actions are taken and when.
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['action_time', 'user', 'content_type', 'object_repr', 'action_flag']
+    list_filter = ['action_flag', 'user']
+
+
+# Customize the admin site header and title
+admin.site.site_header = "FarmiGo"         # Title at the top of the page
+admin.site.site_title = "FarmiGo Admin Portal"   # Title on the browser tab
+admin.site.index_title = "Welcome to FarmiGo"  # Title on the dashboard index page
+
--- server/FarmiGo/models.py
+++ server/FarmiGo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models
+class Vegetable(models.Model):
+    veg_id = models.IntegerField()
+    veg_name = models.CharField(max_length=100)
+    veg_image = models.ImageField(upload_to='vegetable_images/')
+
+    def __str__(self):
+        return self.veg_name
